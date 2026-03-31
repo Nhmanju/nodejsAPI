9bef58a1fa6781bc103b6116a6116781c0100841
Ubuntu <ubuntu@ip-172-31-72-158.ec2.internal>
2026-03-31 14:51:17 +0000

    node js project for docker

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let users = [];
+
+// Health check
+app.get("/", (req, res) => {
+  res.send("App is running 🚀");
+});
+
+// Get users
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// Create user
+app.post("/users", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.status(201).json(user);
+});
+
+//health check
+app.get("/health",(req,res) =>{
+	res.send("status:ok");
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
